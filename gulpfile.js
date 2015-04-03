8ebb402a7094da9eb7e32c2e7c95b6682a6ec49f
Simon Kjellberg <simon.kjellberg@gmail.com>
2015-04-04 01:01:19 +0200

    Add missing build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,52 @@
-var gulp = require('gulp');
-var haml = require('gulp-haml');
+var gulp    = require('gulp');
+var haml    = require('gulp-haml');
+var sass    = require('gulp-sass');
+var inline  = require('gulp-inline');
+var uglify  = require('gulp-uglify');
+var cssmin  = require('gulp-cssmin');
+var htmlmin = require('gulp-htmlmin');
 
-gulp.task('haml', function() {
-  gulp.src('./src/*.haml')
+var hamlTask = function() {
+  return gulp.src('./src/*.haml')
     .pipe(haml())
     .pipe(gulp.dest('./public'));
-});
+};
 
-gulp.task('watch', function() {
-  gulp.run('default');
+gulp.task('haml', hamlTask);
+
+var sassTask = function() {
+  return gulp.src('./src/scss/*.scss')
+    .pipe(sass({
+      includePaths: ['./node_modules']
+    }))
+    .pipe(gulp.dest('./public/css'));
+};
+
+gulp.task('sass', sassTask);
+
+var htmlTask = function() {
+  return gulp.src('./public/*.html')
+    .pipe(htmlmin())
+    .pipe(gulp.dest('./public'));
+};
+
+gulp.task('html', ['sass', 'haml'], htmlTask);
+
+var buildTask = function() {
+  sassTask();
+  hamlTask();
+  htmlTask();
+};
+
+gulp.task('build', buildTask);
+
+var watchTask = function() {
+  buildTask();
+
+  gulp.watch('./src/scss/*.scss', ['sass']);
   gulp.watch('./src/*.haml', ['haml']);
-});
+};
 
-gulp.task('default', function() {
-  gulp.run('haml');
-});
+gulp.task('watch', watchTask);
+
+gulp.task('default', buildTask);
