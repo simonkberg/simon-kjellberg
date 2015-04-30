c9f8007873d15683291621478e7c86165d850777
Simon Kjellberg <simon@lifelike.se>
2015-04-30 19:32:46 +0200

    Add favicon

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ app.set('view engine', 'jade');
 // locals setup
 app.locals.newrelic = newrelic;
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
