3162050fb37aa8c30c7da7ec48368ff72fc5d0ab
Simon Kjellberg <simon@lifelike.se>
2015-04-30 19:13:48 +0200

    Add compression middleware

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var compression = require('compression');
 var sassMiddleware = require('node-sass-middleware');
 
 var routes = require('./routes/index');
@@ -26,6 +27,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 
 app.use(
   sassMiddleware({
