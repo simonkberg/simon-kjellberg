197b9171e63fb2bc462b33d9b4efe0ad36eda072
Simon Kjellberg <simon.kjellberg@gmail.com>
2016-04-21 11:34:43 +0200

    Add GA tracking :shipit:

--- app/client.js
+++ app/client.js
@@ -5,8 +5,11 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, match, browserHistory as history } from 'react-router'
 import Iso from 'iso'
+import ga from 'react-ga'
 import alt from './alt-flux'
 import routes from './routes'
+
+ga.initialize('UA-2241753-14')
 
 match({ history, routes }, (err, redirectLocation, renderProps) => {
   if (err) throw err
@@ -20,6 +23,10 @@ match({ history, routes }, (err, redirectLocation, renderProps) => {
 
     renderProps.createElement = function createElement (Component, props) {
       return <Component context={context} {...props} />
+    }
+
+    renderProps.onUpdate = function trackPageView () {
+      ga.pageview(this.state.location.pathname)
     }
 
     render(<Router {...renderProps} />, container)
