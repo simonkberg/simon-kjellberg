78f49fe261343bfe3ebc83641af3ed82e61b2f60
Simon Kjellberg <simon.kjellberg@gmail.com>
2016-04-27 12:29:33 +0200

    Update meta and add share image

--- app/components/app/App.js
+++ app/components/app/App.js
@@ -3,6 +3,7 @@ import Helmet from 'react-helmet'
 
 import favicons, { appleTouchIcons } from 'helpers/favicon'
 import withUrl from 'helpers/withUrl'
+import share from 'shared/assets/share.png'
 import styles from './App.css'
 
 const { shape, node, func, string } = PropTypes
@@ -48,9 +49,12 @@ class App extends Component {
       meta: [
         { name: 'theme-color', content: '#000000' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'title', property: 'og:title', itemprop: 'name', content: 'Simon Kjellberg' },
         { name: 'description', property: 'og:description', itemprop: 'description', content: 'Creative full stack developer located in Stockholm, Sweden.' },
         { property: 'og:type', content: 'website' },
-        { property: 'og:url', itemprop: 'url', content: url }
+        { property: 'og:url', itemprop: 'url', content: url },
+        { property: 'og:image', itemprop: 'image', content: `/${share}` },
+        { property: 'fb:app_id', content: '1717386788525340' }
       ],
       link: [
         ...appleTouchIcons([152, 144, 120, 114, 72, 57]),
