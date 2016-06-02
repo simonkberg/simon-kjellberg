5bd036c3ce27d5c074a4baf2edc397b8562a8388
Simon Kjellberg <simon.kjellberg@gmail.com>
2016-06-02 15:03:58 +0200

    Remove pointless request cache

--- server/api/waka-time.js
+++ server/api/waka-time.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const request = require('request')
-const cache = require('cached-request')
 
 const JSON_STATS = 'https://wakatime.com/@simonkberg/4a1baa98-ab8f-436e-ada0-8810ef941f76.json'
 const JSON_ACTIVITY = 'https://wakatime.com/@simonkberg/bb9d21e6-a93d-4f64-b741-8a3f0b032fce.json'
@@ -11,13 +10,7 @@ router.get('/stats', (req, res) => wakaTimeProxy(JSON_STATS, res))
 router.get('/activity', (req, res) => wakaTimeProxy(JSON_ACTIVITY, res))
 
 function wakaTimeProxy (url, res) {
-  const opts = {
-    url: url,
-    ttl: 3600,
-    json: true
-  }
-
-  cache(request)(opts, (err, response, body) => {
+  request({ url, json: true }, (err, response, body) => {
     if (err) throw err
 
     res.json(body)
