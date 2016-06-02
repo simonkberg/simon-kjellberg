a93620155af72814d91c6e7ac0379b39ff2f520e
Simon Kjellberg <simon.kjellberg@gmail.com>
2016-06-02 18:40:09 +0200

    Escape link syntax inside code tags

--- src/helpers/slackMessage/parser/textParser.js
+++ src/helpers/slackMessage/parser/textParser.js
@@ -1,5 +1,12 @@
+const UID = Math.floor(Math.random() * 0x10000000000).toString(16)
+const escapedTags = new Set()
+
 // order is important!
 const formats = [
+  // code
+  [/`(.+?)`/g, (match, code) => {
+    return escapeTags(`<code>${escapeCode(code)}</code>`, 'code')
+  }],
   // links
   [/<([!|#|@])?(.+?)(?:\|(.+?))?>/g, (match, prefix, link, title) => {
     title = title || link
@@ -12,8 +19,6 @@ const formats = [
 
     return `<a href="${link}">${title}</a>`
   }],
-  // code
-  [/`(.+?)`/g, (match, code) => `<code>${escapeCode(code)}</code>`],
   // bold
   [/\*(.+?)\*/g, '<strong>$1</strong>'],
   // italic
@@ -22,10 +27,35 @@ const formats = [
   [/~(.+?)~/g, '<s>$1</s>']
 ]
 
+const escapeTags = (string, tag) => {
+  if (!escapedTags.has(tag)) {
+    escapedTags.add(tag)
+  }
+
+  const regex = new RegExp(`<(\/)?(${tag})>`, 'g')
+
+  return string.replace(regex, `@--$1${UID}$2--@`)
+}
+
+const unescapeTags = (string) => {
+  if (escapedTags.size > 0) {
+    const tags = [...escapedTags.values()].join('|')
+    const regex = new RegExp(`@--(\/)?${UID}(${tags})--@`, 'g')
+
+    string = string.replace(regex, '<$1$2>')
+
+    escapedTags.clear()
+  }
+
+  return string
+}
+
 const escapeCode = (string) => {
   return string.replace(/([:*_~`])/g, (match, p1) => `&#${p1.charCodeAt()};`)
 }
 
 export default function textParser (string) {
-  return formats.reduce((string, format) => string.replace(...format), string)
+  return unescapeTags(
+    formats.reduce((string, format) => string.replace(...format), string)
+  )
 }
