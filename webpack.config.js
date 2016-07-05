c5e7143e7d551f7742ee016b1379a5f4b2bab646
Simon Kjellberg <simon.kjellberg@gmail.com>
2016-07-05 16:30:47 +0200

    Improve webpack configurations and structure

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+
+const clientConfig = require('./webpack/config.client')
+const serverConfig = require('./webpack/config.server')
+
+module.exports = function webpackConfig (opts = {}) {
+  // Default options
+  opts = Object.assign({
+    env: process.env.NODE_ENV,
+  }, opts)
+
+  return [
+    clientConfig(opts),
+    serverConfig(opts),
+  ]
+}
