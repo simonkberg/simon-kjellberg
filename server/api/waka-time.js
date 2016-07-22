a899c511a24cd886e5eed461f5a17100d024a785
Simon Kjellberg <simon.kjellberg@gmail.com>
2016-07-22 19:58:47 +0200

    Add caching for stats api

--- server/api/waka-time.js
+++ server/api/waka-time.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const request = require('request')
+const Cache = require('async-cache')
 
 const JSON_STATS = 'https://wakatime.com/@simonkberg/4a1baa98-ab8f-436e-ada0-8810ef941f76.json'
 const JSON_ACTIVITY = 'https://wakatime.com/@simonkberg/bb9d21e6-a93d-4f64-b741-8a3f0b032fce.json'
@@ -9,11 +10,19 @@ const router = express.Router()
 router.get('/stats', (req, res) => wakaTimeProxy(JSON_STATS, res))
 router.get('/activity', (req, res) => wakaTimeProxy(JSON_ACTIVITY, res))
 
+const stats = new Cache({
+  max: 2,
+  maxAge: 1000 * 60 * 60 * 6, // 6 hours
+  load: (url, cb) => request({ url, json: true }, (err, res, body) => {
+    cb(err, body)
+  }),
+})
+
 function wakaTimeProxy (url, res) {
-  request({ url, json: true }, (err, response, body) => {
+  stats.get(url, (err, data) => {
     if (err) throw err
 
-    res.json(body)
+    res.json(data)
   })
 }
 
