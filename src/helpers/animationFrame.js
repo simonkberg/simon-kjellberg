3054543baa58c473f80b2872f213162c9e418619
Simon Kjellberg <simon.kjellberg@gmail.com>
2016-07-22 20:55:00 +0200

    Add rAF helper module

---
+++ src/helpers/animationFrame.js
@@ -0,0 +1,46 @@
+import raf from 'raf'
+
+export default function animationFrame (fn) {
+  let frame = {
+    id: 0,
+    cancel () {
+      raf.cancel(this.id)
+    },
+  }
+
+  let cb = (ts) => {
+    frame.id = fn(ts, frame) && raf(cb)
+  }
+
+  frame.id = raf(cb)
+
+  return frame
+}
+
+/*
+// Stop running frame after 3 seconds
+
+let counter = 0
+let frame = animationFrame(() => {
+  console.log('animationFrame', counter)
+
+  return ++counter
+})
+
+setTimeout(() => {
+  frame.cancel()
+  console.log(frame)
+}, 3000)
+*/
+
+/*
+// Stop from inside frame
+
+let counter = 0
+
+animationFrame(() => {
+  console.log('animationFrame', counter)
+
+  return ++counter < 100
+})
+*/
