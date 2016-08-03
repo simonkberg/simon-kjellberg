0d6fe598386c272ddb8d3183786ffdb22dec1e0b
Simon Kjellberg <simon.kjellberg@gmail.com>
2016-08-03 13:22:52 +0200

    Convert SlackMessgae into functional component

--- src/helpers/slackMessage/Component.js
+++ src/helpers/slackMessage/Component.js
@@ -1,50 +1,45 @@
 import React, { Component, PropTypes } from 'react'
-import shallowCompare from 'react-addons-shallow-compare'
 import parser, { parserCache, emojiCache, cacheShape } from './parser'
 
 const { string } = PropTypes
 
-export default class SlackMessage extends Component {
-  static propTypes = {
-    children: string,
-    component: string,
-    parserCache: cacheShape,
-    emojiCache: cacheShape,
-    emojiCdn: string,
-    emojiClassName: string,
+const SlackMessage = ({
+  component: Component,
+  children,
+  parserCache,
+  emojiCache,
+  emojiCdn,
+  emojiClassName,
+  ...props,
+}) => {
+  const parserOptions = {
+    parser: { cache: parserCache },
+    emoji: {
+      cache: emojiCache,
+      cdnUrl: emojiCdn,
+      className: emojiClassName,
+    },
   }
 
-  static defaultProps = {
-    children: '',
-    component: 'span',
-    parserCache,
-    emojiCache,
-  }
+  const __html = children && parser(children, parserOptions) || ''
 
-  shouldComponentUpdate (nextProps, nextState) {
-    return shallowCompare(this, nextProps, nextState)
-  }
-
-  render () {
-    const {
-      component: Component,
-      children,
-      parserCache,
-      emojiCache,
-      emojiCdn,
-      emojiClassName,
-      ...props,
-    } = this.props
-
-    const __html = children && parser(children, {
-      parser: { cache: parserCache },
-      emoji: {
-        cache: emojiCache,
-        cdnUrl: emojiCdn,
-        className: emojiClassName,
-      },
-    }) || ''
-
-    return <Component dangerouslySetInnerHTML={{__html}} {...props} />
-  }
+  return <Component dangerouslySetInnerHTML={{__html}} {...props} />
 }
+
+SlackMessage.propTypes = {
+  children: string,
+  component: string,
+  parserCache: cacheShape,
+  emojiCache: cacheShape,
+  emojiCdn: string,
+  emojiClassName: string,
+}
+
+SlackMessage.defaultProps = {
+  children: '',
+  component: 'span',
+  parserCache,
+  emojiCache,
+}
+
+export default SlackMessage
