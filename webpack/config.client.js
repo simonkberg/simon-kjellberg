9125239f4563d5cca137ea124e6deeb6a16097d1
Simon Kjellberg <simon.kjellberg@gmail.com>
2016-12-14 14:00:35 +0100

    Add webpack bundle analyzer

--- webpack/config.client.js
+++ webpack/config.client.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack')
 const path = require('path')
 const AssetsPlugin = require('assets-webpack-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const sharedConfig = require('./config.shared')
 
 const { CommonsChunkPlugin } = webpack.optimize
@@ -12,6 +13,7 @@ module.exports = function clientConfig (opts = {}) {
     browser: true,
   }, opts)
 
+  const isDev = opts.env !== 'production'
   const config = sharedConfig(opts)
 
   return Object.assign(config, {
@@ -21,12 +23,15 @@ module.exports = function clientConfig (opts = {}) {
     entry: {
       client: getEntry(
         path.join(paths.src, 'client.js'),
-        opts.env !== 'production'
+        isDev
       ),
       vendor: [
         'react',
         'react-dom',
         'react-router',
+        'react-motion',
+        'react-redux',
+        'react-helmet',
         'immutable',
         'react-ga',
       ],
@@ -42,6 +47,10 @@ module.exports = function clientConfig (opts = {}) {
         name: 'vendor',
         filename: '[name].[hash].js',
         minChunks: Infinity,
+      }),
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        openAnalyzer: false,
       }),
     ],
   })
