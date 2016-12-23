415276ee643fca182792e6d3c0bcd1bcb4a8c08f
Simon Kjellberg <simon.kjellberg@gmail.com>
2016-12-23 17:13:21 +0100

    Minor fixes and legacy changes

--- src/root/configureStore.js
+++ src/root/configureStore.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { batchedSubscribe } from 'redux-batched-subscribe'
-import { unstable_batchedUpdates as batchedUpdates } from 'react-dom' // eslint-disable-line camelcase
+import { unstable_batchedUpdates as batchedUpdates } from 'react-dom'
 import thunkMiddleware from 'redux-thunk'
 import Immutable from 'immutable'
 import rootReducer from './reducers'
@@ -38,8 +38,7 @@ export default function configureStore (initialState = Immutable.Map()) {
   if (__DEV__ && module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('./reducers', () => {
-      const nextRootReducer = require('./reducers').default
-      store.replaceReducer(nextRootReducer)
+      store.replaceReducer(rootReducer)
     })
   }
 
