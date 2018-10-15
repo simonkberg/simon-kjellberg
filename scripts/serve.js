83f5a777f5667090b1afd3901e220ada8c1c972e
Simon Kjellberg <simon.kjellberg@gmail.com>
2018-10-15 16:47:13 +0200

    chore: move files around until it feels right (#160)

---
+++ scripts/serve.js
@@ -0,0 +1,29 @@
+// @flow strict
+
+'use strict'
+
+process.env.NODE_ENV = 'production'
+
+const perf = require('../lib/perf')
+
+perf.mark('boot start')
+
+const next = require('next')
+const options = require('../lib/options')
+const createServer = require('../lib/createServer')
+
+/*::
+import type { Options } from '../lib/options'
+*/
+
+module.exports = options(async (opts /*: Options */) => {
+  const app = next({ dev: false, dir: './src' })
+  const server = await createServer(app)
+
+  server.listen(opts.port, opts.host, undefined, () => {
+    perf.mark('boot end')
+    perf.measure('boot', 'boot start', 'boot end')
+
+    console.log(`🚀 Ready on http://localhost:${opts.port}`)
+  })
+})
