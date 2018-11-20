7e641e23c1405e5346883915a7299726cc0ceb35
Simon Kjellberg <simon.kjellberg@gmail.com>
2018-11-20 22:18:35 +0100

    feat: upgrade to react alpha and implement hooks (#236)

---
+++ src/hooks/useNetworkStatus.js
@@ -0,0 +1,27 @@
+// @flow strict
+import * as React from 'react'
+
+const useNetworkStatus = (
+  initialValue?: boolean = typeof navigator !== 'undefined'
+    ? navigator.onLine
+    : true
+) => {
+  const [value, setValue] = (React: $FlowFixMe).useState(initialValue)
+
+  ;(React: $FlowFixMe).useEffect(() => {
+    const onOnline = () => setValue(true)
+    const onOffline = () => setValue(false)
+
+    window.addEventListener('online', onOnline, false)
+    window.addEventListener('offline', onOffline, false)
+
+    return () => {
+      window.removeEventListener('online', onOnline)
+      window.removeEventListener('offline', onOffline)
+    }
+  }, [])
+
+  return value
+}
+
+export default useNetworkStatus
