cb6278eaeb64239c1ae905903890676b39ed2cd6
Simon Kjellberg <simon.kjellberg@gmail.com>
2019-03-05 23:03:48 +0200

    fix: revert ScrollPreserver hook implementation (#442)
    turns out `getSnapshotBeforeUpdate` actually _can't_ be implemented in hooks yet.

---
+++ src/components/ScrollPreserver.js
@@ -0,0 +1,59 @@
+// @flow strict
+import * as React from 'react'
+import nextTick from '../utils/nextTick'
+
+type Props = {
+  children: <T: React.ElementType>(ref: {
+    current: null | React.ElementRef<T>,
+  }) => React.Node,
+}
+
+export default class ScrollPreserver extends React.Component<Props> {
+  ref = React.createRef<HTMLElement | null>()
+
+  componentDidMount() {
+    const el = this.ref.current
+
+    if (el != null) {
+      nextTick(() => {
+        el.scrollTop = el.scrollHeight
+      })
+    }
+  }
+
+  getSnapshotBeforeUpdate() {
+    const el = this.ref.current
+
+    if (el != null) {
+      const { scrollTop, scrollHeight, clientHeight } = el
+
+      if (scrollTop < scrollHeight - clientHeight) {
+        return scrollHeight - scrollTop
+      }
+    }
+
+    return null
+  }
+
+  componentDidUpdate(
+    prevProps: Props,
+    prevState: void,
+    snapshot: number | null
+  ) {
+    const el = this.ref.current
+
+    if (el != null) {
+      if (snapshot != null) {
+        el.scrollTop = el.scrollHeight - snapshot
+      } else {
+        nextTick(() => {
+          el.scrollTop = el.scrollHeight
+        })
+      }
+    }
+  }
+
+  render() {
+    return this.props.children(this.ref)
+  }
+}
